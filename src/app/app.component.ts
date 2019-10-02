f7ef0ef2f4eea59ef013a7a8db747f95cc08035a
Gyanprakash <gpp23aug@gmail.com>
2019-10-02 23:52:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'highlight-on-select-row';
-}
+  heading: string;
+  names: { name: string; age: string}[];
+  setClickedRow: number = null;
+  constructor() {
+      this.heading = "Please click below row";
+      this.names = [{
+          name : "Malcum",
+          age: "23",
+      },
+      {
+          name : "jhon",
+          age: " 30",
+      },
+      {
+          name : "rudy",
+          age: "22",
+      },
+      {
+          name : "mathew",
+          age: "34",
+      }];
+    }
+
+    setClickedRow(index){
+        this.selectedRow = index;
+    }
+  }
